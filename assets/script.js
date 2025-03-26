928fde8651206df65518b2f639e72a493f0de475
medba <med6ba@gmail.com>
2025-03-26 11:07:23 +0000

    update files

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,46 @@
+// theme toggle button
+
 document.addEventListener("DOMContentLoaded", function () {
   const themeToggle = document.getElementById("theme-toggle");
   const themeIcon = document.getElementById("theme-icon");
   const htmlElement = document.documentElement;
 
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "light") {
+    htmlElement.classList.add("light-mode");
+    themeIcon.classList.remove("fa-sun");
+    themeIcon.classList.add("fa-moon");
+  } else {
+    htmlElement.classList.remove("light-mode");
+    themeIcon.classList.remove("fa-moon");
+    themeIcon.classList.add("fa-sun");
+  }
+
   themeToggle.addEventListener("click", function () {
     // Toggle the light mode
     htmlElement.classList.toggle("light-mode");
 
-    // Toggle the icon between moon and sun
     if (htmlElement.classList.contains("light-mode")) {
+      localStorage.setItem("theme", "light");
       themeIcon.classList.remove("fa-sun");
       themeIcon.classList.add("fa-moon");
     } else {
+      localStorage.setItem("theme", "dark");
       themeIcon.classList.remove("fa-moon");
       themeIcon.classList.add("fa-sun");
     }
+  });
+});
+
+// Scroll to top button
+
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopBtns = document.querySelectorAll(".back-to-top");
+
+  backToTopBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
   });
 });
